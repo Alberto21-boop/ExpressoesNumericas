82af1598fdc688057d38566a086e6362057088e5
alberto <albertobarbosa0003@gmail.com>
2022-05-15 11:23:15 -0300

    Adicionar arquivos de projeto.

---
+++ ExpressoesNumericas/Expressoes.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpressoesNumericas;
+
+public class Expressoes
+{
+    public static void Main(string[] args)
+    {
+        //80-6x7+5 = ...
+        int a = 80 - 6 * 7 + 5;
+        Console.WriteLine(a);
+
+        // (+80) + (-60) + (-100) = ...
+        int b = 80 + (-60) + (-100);
+        Console.WriteLine(b);
+
+        // 4+[12-(2+5)+9]
+        int c = 4 + (12 - (2 + 5) + 9);
+        Console.WriteLine(c);
+    }
+}
